0d0a486c21ad3c4f2ca0478dc411a57820bc2b47
venerayan <venerayan@gmail.com>
2023-10-23 01:41:10 +0000

    refactor mail

--- main.go
+++ main.go
@@ -8,27 +8,12 @@ import (
 	"net/http"
 	"net/smtp"
 	"os"
+	"github.com/rv-openshift/webhook-rate/mail"
 )
 
 type Message struct {
 	Status string `json:"status"`
 	Body   string `json:"body"`
-}
-
-func SendmaiL(email string, password string) error {	
-	to := []string{
-		email,
-	}
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
-	message := []byte("Subject: Test-email!\r\n"+"This is a test email message.\r\n")
-	auth := smtp.PlainAuth("", email, password, smtpHost)
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, "no-reply@gmail.com", to, message)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Email Sent Successfully!")
-	return err
 }
 
 func writeConsole(writer http.ResponseWriter, content, status, body string, fail bool) error  {
